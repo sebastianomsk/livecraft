d5b55f4dbbacc16e33eb5df9472c14277718d921
avila sebastian <sebastian@coltech.com.ar>
2022-08-15 16:21:55 -0300

    Implementing chunks

--- 3DLivecraft.js
+++ 3DLivecraft.js
@@ -6,6 +6,7 @@ const PIXEL_BLOCK = 5;
 noise.seed(Math.random())
 
 let scene = new THREE.Scene();
+scene.background = new THREE.Color(0x00ffff);
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize(windowWidth, windowHeight);
 
@@ -60,26 +61,60 @@ let materiaArray = [
 // 
 // Renderizado del terreno, bloques
 // 
-let blocks = [];
+// let blocks = [];
+// let xoff = 0;
+// let zoff = 0;
+// let inc = 0.05;
+// let amplitude = 100;
+// for (let x = 0; x < 20; x++) {
+//     xoff = 0;
+//     for (let z = 0; z < 20; z++) {
+//         // Masa densidad
+//         let v = Math.round(noise.perlin2(xoff, zoff) * amplitude / 5) * 5;
+//         // let v = Math.round(noise.perlin2(x, z) * amplitude / 5) * 5;
+//         blocks.push(new Block(x * 5, v, z * 5));
+//         xoff = xoff + inc;
+//     }
+//     zoff = zoff + inc;
+// }
+
+// 
+// render chunks
+// 
+let chunks = [];
 let xoff = 0;
 let zoff = 0;
 let inc = 0.05;
-let amplitude = 100;
-for (let x = 0; x < 20; x++) {
-    xoff = 0;
-    for (let z = 0; z < 20; z++) {
-        // Masa densidad
-        let v = Math.round(noise.perlin2(xoff, zoff) * amplitude / 5) * 5;
-        // let v = Math.round(noise.perlin2(x, z) * amplitude / 5) * 5;
-        blocks.push(new Block(x * 5, v, z * 5));
-        xoff = xoff + inc;
+let amplitude = 30 + (Math.random() * 70);
+let renderDistance = 3;
+let chunkSize = 10;
+// start in the middle of map
+camera.position.x = renderDistance * chunkSize / 2 * 5;
+camera.position.z = renderDistance * chunkSize / 2 * 5;
+camera.position.y = 50;
+
+for (let i = 0; i < renderDistance; i++) {
+    let chunk = [];
+    for (let j = 0; j < renderDistance; j++) {
+        for (let x = i * chunkSize; x < (i * chunkSize) + chunkSize; x++) {
+            for (let z = j * chunkSize; z < (j * chunkSize) + chunkSize; z++) {
+                xoff = inc * x;
+                zoff = inc * z;
+                let v = Math.round(noise.perlin2(xoff, zoff) * amplitude / 5) * 5;
+                chunk.push(new Block(x * 5, v, z * 5));
+            }
+        }
+        
     }
-    zoff = zoff + inc;
+    chunks.push(chunk);
 }
 
-for (let i = 0; i < blocks.length; i++) {
+for (let i = 0; i < chunks.length; i++) {
     // const block = blocks[i];
-    blocks[i].display();
+    // blocks[i].display();
+    for (let j = 0; j < chunks[i].length; j++) {
+        chunks[i][j].display();
+    }
 }
 
 /**
@@ -154,13 +189,15 @@ function update() {
     if (keys.includes("w")) {
         controls.moveForward(movingSpeed);
         if (!autoJump) {
-            for (let i = 0; i < blocks.length; i++) {
-                const block = blocks[i];
-                // Check if the player is on top of a block
-                if (wantToMove(block)) {
-                    // Check if the player is on under of a block
-                    if (camera.position.y == block.y - halfPixelBlock) {
-                        controls.moveForward(-1 * movingSpeed);
+            for (let i = 0; i < chunks.length; i++) {
+                for (let j = 0; j < chunks[i].length; j++) {
+                    const block = chunks[i][j];
+                    // Check if the player is on top of a block
+                    if (wantToMove(block)) {
+                        // Check if the player is on under of a block
+                        if (camera.position.y == block.y - halfPixelBlock) {
+                            controls.moveForward(-1 * movingSpeed);
+                        }
                     }
                 }
             }
@@ -169,8 +206,9 @@ function update() {
     if (keys.includes("a")) {
         controls.moveRight(-1 * movingSpeed);
         if (!autoJump) {
-            for (let i = 0; i < blocks.length; i++) {
-                const block = blocks[i];
+            for (let i = 0; i < chunks.length; i++) {
+                for (let j = 0; j < chunks[i].length; j++) {
+                const block = chunks[i][j];
                 // Check if the player is on top of a block
                 if (wantToMove(block)) {
                     // Check if the player is on under of a block
@@ -179,18 +217,21 @@ function update() {
                     }
                 }
             }
+            }
         }
     }
     if (keys.includes("s")) {
         controls.moveForward(-1 * movingSpeed);
         if (!autoJump) {
-            for (let i = 0; i < blocks.length; i++) {
-                const block = blocks[i];
-                // Check if the player is on top of a block
-                if (wantToMove(block)) {
-                    // Check if the player is on under of a block
-                    if (camera.position.y == block.y - halfPixelBlock) {
-                        controls.moveForward(movingSpeed);
+            for (let i = 0; i < chunks.length; i++) {
+                for (let j = 0; j < chunks[i].length; j++) {
+                    const block = chunks[i][j];
+                    // Check if the player is on top of a block
+                    if (wantToMove(block)) {
+                        // Check if the player is on under of a block
+                        if (camera.position.y == block.y - halfPixelBlock) {
+                            controls.moveForward(movingSpeed);
+                        }
                     }
                 }
             }
@@ -199,13 +240,15 @@ function update() {
     if (keys.includes("d")) {
         controls.moveRight(movingSpeed);
         if (!autoJump) {
-            for (let i = 0; i < blocks.length; i++) {
-                const block = blocks[i];
-                // Check if the player is on top of a block
-                if (wantToMove(block)) {
-                    // Check if the player is on under of a block
-                    if (camera.position.y == block.y - halfPixelBlock) {
-                        controls.moveRight(-1 * movingSpeed);
+            for (let i = 0; i < chunks.length; i++) {
+                for (let j = 0; j < chunks[i].length; j++) {
+                    const block = chunks[i][j];
+                    // Check if the player is on top of a block
+                    if (wantToMove(block)) {
+                        // Check if the player is on under of a block
+                        if (camera.position.y == block.y - halfPixelBlock) {
+                            controls.moveRight(-1 * movingSpeed);
+                        }
                     }
                 }
             }
@@ -217,19 +260,34 @@ function update() {
 
     // Check if the camere is like collinding ok with the block
     // Check if person hit the block. Then up or down next block
-    for (let i = 0; i < blocks.length; i++) {
-        const block = blocks[i];
-        // Check if the player is on top of a block
-        if (wantToMove(block)) {
-            // Check if the player is on under of a block
-            if (camera.position.y <= block.y + halfPixelBlock && camera.position.y >= block.y - halfPixelBlock) {
-                camera.position.y = block.y + halfPixelBlock;
-                ySpeed = 0;//Stop move
-                canJump = true;
-                break;
+    for (const blocks of chunks) {
+        for (const block of blocks) {
+            if (wantToMove(block)) {
+                // Check if the player is on under of a block
+                if (camera.position.y <= block.y + halfPixelBlock && camera.position.y >= block.y - halfPixelBlock) {
+                    camera.position.y = block.y + halfPixelBlock;
+                    ySpeed = 0;//Stop move
+                    canJump = true;
+                    break;
+                }
             }
         }
     }
+    // for (let i = 0; i < chunks.length; i++) {
+    //     for (let j = 0; j < chunks[i].length; j++) {
+    //         const block = chunks[i][j];
+    //         // Check if the player is on top of a block
+    //         if (wantToMove(block)) {
+    //             // Check if the player is on under of a block
+    //             if (camera.position.y <= block.y + halfPixelBlock && camera.position.y >= block.y - halfPixelBlock) {
+    //                 camera.position.y = block.y + halfPixelBlock;
+    //                 ySpeed = 0;//Stop move
+    //                 canJump = true;
+    //                 break;
+    //             }
+    //         }
+    //     }
+    // }
 }
 
 // player want to move himself
